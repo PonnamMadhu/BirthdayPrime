d84feb199b81895d5f2a4f92123f9244c3c18739
Madhu <madhuponnam66@gmail.com>
2021-11-25 21:38:24 +0530

    fix

--- api.js
+++ api.js
@@ -2,17 +2,34 @@ var btnTRanslate=document.querySelector("#btn-translate");
 var textInput=document.querySelector("#input");
 var output=document.querySelector("#output");
 function clickHandler(){
-   for(var i=2;i<textInput.value;i++)
-   {
-       if(textInput.value%i==0){
-           output.innerHTML=("Your birthday is Not  a Prime number");
-           
-       }
-       else
-       {
-        output.innerHTML=("Your birtday is Prime Number")
-       }
-   }
-       
+    
+    var a =textInput.value;
+    var newStr=a.replaceAll("-","");
+   addition(newStr)
+   
+}
+function addition(str){
+    
+    var ar=str.split("")
+    let sum=0
+    for(let i=0;i<ar.length;i++){
+        sum+=Number(ar[i])
+    }
+    console.log(sum)
+    isPrime(sum)
+}
+function isPrime(num){
+    let c =true;
+    for(let i=2;i<num;i++){
+        if(num%i==0){
+            c=false;
+            break;
+        }
+    }
+    if(c){
+        output.innerHTML="Yayy! Your Birthday is a Prime Number.🥳🥳"
+    }else{
+        output.innerHTML="Oh No! Your Birthday is Not a Prime Number.☹️"
+    }
 }
 btnTRanslate.addEventListener("click",clickHandler)
